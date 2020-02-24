c8cd6997906b835c8b7f1b4460360210bd517aba
Kelly Jung <kellyjung96@gmail.com>
2020-02-23 20:39:05 -0600

    blog data initial

--- kellysjung.com/src/App.tsx
+++ kellysjung.com/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import data from './resumeData.json';
 
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import About from './Components/About';
-import Resume from './Components/Resume';
-import Contact from './Components/Contact';
-import MyStory from './Components/My_Story';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import About from './components/About';
+import Resume from './components/Resume';
+import Contact from './components/Contact';
+import MyStory from './components/My_Story';
 
 interface Props {};
 interface ComponentState {};
--- kellysjung.com/src/Blog.tsx
+++ kellysjung.com/src/Blog.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
-import SecondaryNavMenu from './Components/common/secondary_nav_menu';
+import SecondaryNavMenu from './components/common/secondary_nav_menu';
+import Data from './blogData.json';
 
 interface Props {};
 interface ComponentState {};
 
 export default class Blog extends React.Component<Props, ComponentState> {
     render(): JSX.Element {
+        const FirstPost = Data.Post;
+
+   
         return (
             <section id="blog" className="SecondaryPage">
                 <SecondaryNavMenu CurrentPage='Blog' />
-                <div style={{ textAlign: 'center', height: 900 }}>
+                {/* <div style={{ textAlign: 'center', height: 900 }}>
                     This page is a work in progress! I'll have some of my musings and reflections here.
+                </div> */}
+                <h1><span>WIP</span></h1>
+                <div className='BlogPostsWrapper'>
+                    <div className='BlogPost'>
+                        <div className='PostImage'><img src={FirstPost.Image} /></div>
+                        <div className='PostTitle'>{FirstPost.Title}</div>
+                        <div className='PostDescription'>{FirstPost.Description}</div>
+                        <div className='PostDate'>{FirstPost.Date}</div>
+                    </div>
                 </div>
             </section>
         );
