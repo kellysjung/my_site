cc2b87fc81d12c889e8761126f3ac98b686f3e09
Kelly Jung <kellyjungg96@gmail.com>
2020-12-22 22:11:59 -0500

    ?

--- kellysjung.com/src/index.js
+++ kellysjung.com/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import Router from './main_pages/router';
 import registerServiceWorker from './javascript/registerServiceWorker';
 
-// for if I want to go back to single page
+// For if I want to go back to single page
 // ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<Router />, document.getElementById('root'));
 
