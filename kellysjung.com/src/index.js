4af4306b235ecd76e44a7e85275a7e729a1ef06a
Kelly Jung <kellyjungg96@gmail.com>
2020-12-24 13:36:45 -0500

    clean up

--- kellysjung.com/src/index.js
+++ kellysjung.com/src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import Router from './main_pages/router';
 import registerServiceWorker from './javascript/registerServiceWorker';
 
-// For if I want to go back to single page
-// ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<Router />, document.getElementById('root'));
 
 registerServiceWorker();
